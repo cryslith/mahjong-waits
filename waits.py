6a3a3adef30d9013b53341074d43240843180b31
Lily Chung <lkdc@mit.edu>
2024-09-03 19:51:08 -0400

    compute waits of hand

---
+++ waits.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class Wait:
+    pinfu: bool
+    iipeiko: bool
+
+    def __or__(self, other):
+        return type(self)(self.pinfu or other.pinfu, self.iipeiko or other.iipeiko)
+
+def add_wait(waitset, k, w):
+    if k in waitset:
+        waitset[k] |= w
+    else:
+        waitset[k] = w
+
+@dataclass(frozen=True)
+class State:
+    location: int
+    n1: int # ongoing length-1 runs
+    n2: int # ongoing length-2 runs
+    wait: bool # wait already used before
+    pair: bool # pair already used before
+
+    def score(self, hand):
+        if self.location > len(hand):
+            return {}
+        if self.location == len(hand):
+            if self.n2 > 0 or self.n1 > 1 or not self.pair:
+                return {}
+            if self.n1 == 0:
+                if self.wait:
+                    return {None: Wait(pinfu=True, iipeiko=False)}
+                return {}
+            # n1 == 1
+            if self.wait:
+                return {}
+            return {self.location: Wait(pinfu=True, iipeiko=False)}
+        waitset = {}
+        for use_wait in {False, not self.wait}:
+            for use_pair in {False, not self.pair}:
+                if hand[self.location] + use_wait > 4:
+                    continue
+                leftover = hand[self.location] + use_wait - self.n1 - self.n2 - 2*use_pair
+                if leftover < 0:
+                    continue
+                for use_set in {False, leftover >= 3}:
+                    new_runs = leftover - 3*use_set
+                    next_state = State(
+                        location=self.location + 1,
+                        n1=self.n2,
+                        n2=new_runs,
+                        wait=self.wait or use_wait,
+                        pair=self.pair or use_pair,
+                    )
+                    next_waitset = yield next_state
+                    for k, v in next_waitset.items():
+                        new_k = self.location if use_wait else k
+                        new_v = Wait(
+                            pinfu=not use_set and v.pinfu and (not use_wait or self.n1 > 0 or new_runs > 0),
+                            iipeiko=new_runs >= 2 or v.iipeiko,
+                        )
+                        add_wait(waitset, new_k, new_v)
+        return waitset            
+
+def dp(hand, wait=False, pair=False):
+    outputs = {}
+    stack = []
+
+    state = State(location=0, n1=0, n2=0, wait=wait, pair=pair)
+    gen = None
+    ret = None
+    while True:
+        try:
+            if gen is None:
+                gen = state.score(hand)
+                call = next(gen)
+            else:
+                call = gen.send(ret)
+        except StopIteration as e:
+            ret = e.value
+            outputs[state] = ret
+            if stack:
+                (state, gen) = stack.pop()
+                continue
+            break
+        if call in outputs:
+            ret = outputs[call]
+            continue
+        else:
+            stack.append((state, gen))
+            state = call
+            gen = None
+            ret = None
+            continue
+    return ret
+
+# hand is input as list of number of tiles, starting from 0; waits are returned starting from -1
+def waits(hand):
+    # renumber hand to start from 1, waits start from 0
+    hand = [0] + list(hand) + [0]
+
+    result = dp(hand)
+    # renumber result
+    return {(k-1 if k is not None else None): v for k, v in result.items()}
+
+def main():
+    while True:
+        prompt = 'input hand> '
+        hand = input(prompt)
+        w = waits([int(c) for c in hand])
+        print(' ' * (len(prompt) - 1), end='')
+        for i in range(-1, len(hand) + 1):
+            if i not in w:
+                c = ' '
+            elif w[i].pinfu and w[i].iipeiko:
+                c = 'I'
+            elif w[i].pinfu:
+                c = 'p'
+            elif w[i].iipeiko:
+                c = 'i'
+            else:
+                c = '^'
+            print(c, end='')
+        print()
+
+if __name__ == '__main__':
+    main()
